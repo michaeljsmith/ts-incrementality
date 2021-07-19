0090893fb605aae80f318748bac33d193ba02762
Michael Smith <msmith@msmith.id.au>
2021-07-19 17:11:01 +1000

    Initial commit.

---
+++ src/incrementality.test.ts
@@ -0,0 +1,96 @@
+import { expect } from "chai";
+import { host, incremental } from "./incrementality.js";
+
+type FullName = {
+  first: string;
+  second: string;
+};
+
+let nodesExecuted = 0;
+
+const totalNameLength = incremental(cache =>
+  (fullName: FullName) => {
+    ++nodesExecuted;
+    return nameLength(cache('first'), fullName.first) + nameLength(cache('second'), fullName.second);
+  });
+
+const nameLength = incremental(cache =>
+  (name: string) => {
+    ++nodesExecuted;
+    return name.length;
+  });
+
+describe('filter', function() {
+  it('evaluates result', function() {
+    const fullName = {first: 'Sue', second: 'Sanderson'};
+    const update = host(totalNameLength);
+    expect(update(fullName)).equals(3 + 9);
+  });
+
+  it('caches result', function() {
+    nodesExecuted = 0;
+    let fullName = {first: 'Sue', second: 'Sanderson'};
+    const update = host(totalNameLength);
+    update(fullName);
+    const firstNodesExecuted = nodesExecuted;
+    expect(update(fullName)).equals(3 + 9);
+    expect(nodesExecuted).equals(firstNodesExecuted);
+  });
+
+  it('re-evaluates incrementally', function() {
+    const fullName1 = {first: 'Sue', second: 'Sanderson'};
+    const update = host(totalNameLength);
+    update(fullName1);
+    expect(update(fullName1)).equals(3 + 9);
+    const fullName2 = {first: fullName1.first, second: 'Jenkins'};
+    expect(update(fullName2)).equals(3 + 7);
+  });
+
+  it('throws if function changes', function() {
+    const executesClosure = incremental(cache => 
+      (x: number) => {
+        const invalidIncrementalClosure = incremental(cache2 =>
+          (x2: number) => x2 + 1);
+        return invalidIncrementalClosure(cache('closure'), x * 2);
+      });
+
+    const update = host(executesClosure);
+    update(2);
+    expect(() => update(3)).throws();
+  });
+
+  it('throws if cache key re-used', function() {
+    const reusesCacheKey = incremental(cache => 
+      (fullName: FullName) => {
+        const length0 = nameLength(cache('first'), fullName.first);
+        const length1 = nameLength(cache('first'), fullName.second);
+        return length0 + length1;
+      });
+
+    const update = host(reusesCacheKey);
+    const fullName = {first: 'Sue', second: 'Sanderson'};
+    expect(() => update(fullName)).throws();
+  });
+
+  it('throws if cache key re-used on subsequent call', function() {
+    const doubles = incremental(cache => 
+      (x: number) => {
+        return x * 2;
+      });
+    const reusesCacheKeyOnSubsequentCall = incremental(cache => 
+      (callIndex: number) => {
+        const x0 = doubles(cache('first'), 1);
+        if (callIndex == 0) {
+          return x0;
+        }
+        return x0 + doubles(cache('first'), 2);
+      });
+
+    const update = host(reusesCacheKeyOnSubsequentCall);
+
+    // Due to how test function above is written, error will not be apparent
+    // on the first call.
+    update(0);
+    expect(() => update(1)).throws();
+  });
+});
---
+++ src/incrementality.ts
@@ -0,0 +1,84 @@
+export interface Cache {
+  (key: string): CacheEntry;
+
+  // For detecting tracking duplicate key usage.
+  visitKey: number;
+}
+
+export interface CacheEntry {
+  memoization?: unknown,
+};
+
+type Memoization<T, Args extends [unknown]> = {
+  cache: Cache,
+
+  previousFunction: (cache: Cache) => (...args: Args) => T;
+  previousArgs: Args;
+  previousResult: T;
+}
+
+function newCache(): Cache {
+  const cache = new Map<string, {visitKey: number, entry: CacheEntry}>();
+  const result = (key: string) => {
+    let node = cache.get(key);
+    if (node === undefined) {
+      node = {
+        visitKey: -1,
+        entry: {},
+      }
+      cache.set(key, node);
+    }
+    if (node.visitKey === result.visitKey) {
+      throw `Referenced cache entry ${key} multiple times.`;
+    };
+    node.visitKey = result.visitKey;
+    return node.entry;
+  };
+  result.visitKey = 0;
+
+  return result;
+}
+
+export function host<T, I>(fn: (cacheEntry: CacheEntry, input: I) => T) {
+  const rootCacheEntry: CacheEntry = {};
+  return (input: I): T => {
+    return fn(rootCacheEntry, input);
+  }
+}
+
+export function incremental<T, Args extends [unknown]>(
+  fn: (cache: Cache) => (...args: Args) => T)
+: (cacheEntry: CacheEntry, ...args: Args) => T {
+  return (cacheEntry, ...args) => {
+    let memoization = cacheEntry.memoization as Memoization<T, Args> | undefined;
+    if (memoization !== undefined) {
+      // If the previous function is different to this one, then we may be passing a closure
+      // to this function - in this case we can't be sure that the result of the function
+      // depends solely on the passed-in arguments, so to be safe we throw an exception here.
+      if (memoization.previousFunction !== fn) {
+        throw 'Cached function differs from previous invocation - may be trying to cache a closure';
+      }
+
+      // If the arguments are all the same, then we can re-use the result.
+      if (memoization.previousArgs.reduce((accumulator, arg, i) =>
+          accumulator && arg === args[i], true)) {
+        return memoization.previousResult;
+      }
+    }
+
+    if (memoization === undefined) {
+      const newMemoization = {} as Memoization<T, Args>;
+      cacheEntry.memoization = memoization = newMemoization;
+      memoization.cache = newCache();
+    }
+    memoization.previousFunction = fn;
+    memoization.previousArgs = args;
+
+    // Increment the visit key, so that one call to each sub-node will be allowed.
+    memoization.cache.visitKey++;
+
+    const result = fn(memoization.cache)(...args);
+    memoization.previousResult = result;
+    return result;
+  };
+}
