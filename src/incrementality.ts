89b892bde0ef24c6298d88cefa001576dd0639b1
Michael Smith <msmith@msmith.id.au>
2021-07-20 16:35:13 +1000

    Disentangle memoization from cache.

--- src/incrementality.ts
+++ src/incrementality.ts
@@ -1,40 +1,34 @@
 export interface Cache {
-  (key: string): CacheEntry;
+  (key: string): CacheReference;
 
   // For detecting tracking duplicate key usage.
   incrementVisitKey(): void;
 }
 
-export interface CacheEntry {
-  memoization?: unknown,
-};
-
-type Memoization<T, Args extends [unknown]> = {
-  cache: Cache,
-
-  previousFunction: (cache: Cache) => (...args: Args) => T;
-  previousArgs: Args;
-  previousResult: T;
+export interface CacheReference {
+  getOrCreate<T extends {}>(init: () => T): T;
 }
 
 function newCache(): Cache {
-  const cache = new Map<string, {visitKey: number, entry: CacheEntry}>();
+  const cache = new Map<string, {visitKey: number, entry: {}}>();
   let visitKey = 0;
-  const result = (key: string) => {
-    let node = cache.get(key);
-    if (node === undefined) {
-      node = {
-        visitKey: -1,
-        entry: {},
+  const result = (key: string) => ({
+    getOrCreate<T extends {}>(init: () => T) {
+      let node = cache.get(key);
+      if (node === undefined) {
+        node = {
+          visitKey: -1,
+          entry: init(),
+        }
+        cache.set(key, node);
       }
-      cache.set(key, node);
-    }
-    if (node.visitKey === visitKey) {
-      throw `Referenced cache entry ${key} multiple times.`;
-    }
-    node.visitKey = visitKey;
-    return node.entry;
-  };
+      if (node.visitKey === visitKey) {
+        throw `Referenced cache entry ${key} multiple times.`;
+      }
+      node.visitKey = visitKey;
+      return node.entry as T;
+      }
+  });
   result.incrementVisitKey = () => {
     ++visitKey;
   };
@@ -42,46 +36,67 @@ function newCache(): Cache {
   return result;
 }
 
-export function host<T, I>(fn: (cacheEntry: CacheEntry, input: I) => T) {
-  const rootCacheEntry: CacheEntry = {};
+export function host<T, I>(fn: (cacheEntry: CacheReference, input: I) => T) {
+  let rootCacheEntry: {} | undefined = undefined;
+  const rootCacheReference: CacheReference = {
+    getOrCreate<T extends {}>(init: () => T): T {
+      if (rootCacheEntry === undefined) {
+        rootCacheEntry = init();
+      }
+      return rootCacheEntry as T;
+    }
+  };
   return (input: I): T => {
-    return fn(rootCacheEntry, input);
+    return fn(rootCacheReference, input);
   }
+}
+
+type Memoization<T, Args extends [unknown]> = {
+  previousFunction: (cache: Cache) => (...args: Args) => T;
+  previousArgs: Args;
+  previousResult: T;
+};
+
+type IncrementalFunctionEntry<T, Args extends [unknown]> = {
+  cache: Cache,
+
+  memoization?: Memoization<T, Args>;
 }
 
 export function incremental<T, Args extends [unknown]>(
   fn: (cache: Cache) => (...args: Args) => T)
-: (cacheEntry: CacheEntry, ...args: Args) => T {
-  return (cacheEntry, ...args) => {
-    let memoization = cacheEntry.memoization as Memoization<T, Args> | undefined;
-    if (memoization !== undefined) {
+: (cacheReference: CacheReference, ...args: Args) => T {
+  return (cacheReference, ...args) => {
+    const entry: IncrementalFunctionEntry<T, Args> = cacheReference.getOrCreate(() => ({
+      cache: newCache(),
+    }));
+    if (entry.memoization !== undefined) {
       // If the previous function is different to this one, then we may be passing a closure
       // to this function - in this case we can't be sure that the result of the function
       // depends solely on the passed-in arguments, so to be safe we throw an exception here.
-      if (memoization.previousFunction !== fn) {
+      if (entry.memoization.previousFunction !== fn) {
         throw 'Cached function differs from previous invocation - may be trying to cache a closure';
       }
 
       // If the arguments are all the same, then we can re-use the result.
-      if (memoization.previousArgs.reduce((accumulator, arg, i) =>
+      if (entry.memoization.previousArgs.reduce((accumulator, arg, i) =>
           accumulator && arg === args[i], true)) {
-        return memoization.previousResult;
+        return entry.memoization.previousResult;
       }
     }
 
-    if (memoization === undefined) {
+    if (entry.memoization === undefined) {
       const newMemoization = {} as Memoization<T, Args>;
-      cacheEntry.memoization = memoization = newMemoization;
-      memoization.cache = newCache();
+      entry.memoization = newMemoization;
     }
-    memoization.previousFunction = fn;
-    memoization.previousArgs = args;
+    entry.memoization.previousFunction = fn;
+    entry.memoization.previousArgs = args;
 
     // Increment the visit key, so that one call to each sub-node will be allowed.
-    memoization.cache.incrementVisitKey();
+    entry.cache.incrementVisitKey();
 
-    const result = fn(memoization.cache)(...args);
-    memoization.previousResult = result;
+    const result = fn(entry.cache)(...args);
+    entry.memoization.previousResult = result;
     return result;
   };
 }
