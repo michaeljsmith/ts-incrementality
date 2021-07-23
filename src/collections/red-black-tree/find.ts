bc9da4821fb23fafd585f94c376c9b91b0517dc0
Michael Smith <msmith@msmith.id.au>
2021-07-23 17:51:33 +1000

    Add find method to rb tree.

---
+++ src/collections/red-black-tree/find.ts
@@ -0,0 +1,20 @@
+import { Comparator, RbNode, RbTree } from "./tree.js";
+
+export function rbFind<K, V>(tree: RbTree<K, V>, key: K, comparator: Comparator<K>): RbNode<K, V> | undefined {
+  if (tree === null) {
+    return undefined;
+  }
+
+  // Check which way to recurse.
+  const comparison = comparator(key, tree.keyValue.key);
+  if (comparison == 0) {
+    // We have found the node to delete.
+    return tree;
+  } else if (comparison < 0) {
+    // Recurse to the left.
+    return rbFind(tree.left, key, comparator);
+  } else {
+    // Recurse to the right.
+    return rbFind(tree.right, key, comparator);
+  }
+}
